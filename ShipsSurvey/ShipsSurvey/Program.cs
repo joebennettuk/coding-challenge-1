1d27a934610eacb1a6aa7c931b3c070d2bcc49e5
Joe <hello@joebennett.co.uk>
2018-04-27 16:24:37 +0100

    Create initial classes and struct; Ship, WorldHelper and Coordinates

--- ShipsSurvey/ShipsSurvey/Program.cs
+++ ShipsSurvey/ShipsSurvey/Program.cs
@@ -6,11 +6,60 @@ using System.Threading.Tasks;
 
 namespace ShipsSurvey
 {
+    static class WorldHelper
+    {
+        public enum Compass
+        {
+            N = 0,
+            E = 90,
+            S = 180,
+            W = 270
+        }
+
+        //rotation in degrees to compass enum
+        public static Compass RotationToCompass(int rotation)
+        {
+            return (Compass)rotation;
+        }
+
+        //string to rotation to compass enum
+        public static Compass CompassToRotation(string direction)
+        {
+            Enum.TryParse(direction.ToUpper(), out Compass c);
+            return c;
+        }
+    }
+
+    public struct Coordinates
+    {
+        public int x, y;
+    }
+
+    public class Ship
+    {
+        public Coordinates location;
+        public Coordinates world;
+        public int rotation;
+
+        public Ship(Coordinates w, int x, int y, int r)
+        {
+            location.x = x;
+            location.y = y;
+            world = w;
+            rotation = r;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello!");
+            Coordinates world = new Coordinates() { x = 5, y = 3 };
+            Ship ship1 = new Ship(world, 1, 1, (int)WorldHelper.CompassToRotation("e"));
+
+            Console.WriteLine(WorldHelper.RotationToCompass(ship1.rotation));
+            Console.WriteLine((int)WorldHelper.CompassToRotation("w"));
+            Console.ReadLine();
         }
     }
 }
