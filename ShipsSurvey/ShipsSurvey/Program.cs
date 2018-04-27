e32241eddc0579754bb2179db2adc851fe306e41
Joe <hello@joebennett.co.uk>
2018-04-27 16:54:02 +0100

    add Left, Right and Forward methods for ship class

--- ShipsSurvey/ShipsSurvey/Program.cs
+++ ShipsSurvey/ShipsSurvey/Program.cs
@@ -38,8 +38,10 @@ namespace ShipsSurvey
     public class Ship
     {
         public Coordinates location;
-        public Coordinates world;
-        public int rotation;
+        Coordinates world;
+        int rotation;
+        Coordinates lostLocation;
+        bool alive = true;
 
         public Ship(Coordinates w, int x, int y, int r)
         {
@@ -47,6 +49,56 @@ namespace ShipsSurvey
             location.y = y;
             world = w;
             rotation = r;
+        }
+
+        public void Left()
+        {
+            if (rotation == 0)
+                rotation = 270;
+            else
+                rotation -= 90;
+        }
+
+        public void Right()
+        {
+            if (rotation == 270)
+                rotation = 0;
+            else
+                rotation += 90;
+        }
+
+        public bool Forward()
+        {
+            //move ship forward in direction
+            switch (WorldHelper.RotationToCompass(rotation))
+            {
+                case WorldHelper.Compass.N:
+                    location.y += 1;
+                    break;
+                case WorldHelper.Compass.E:
+                    location.x += 1;
+                    break;
+                case WorldHelper.Compass.S:
+                    location.y -= 1;
+                    break;
+                case WorldHelper.Compass.W:
+                    location.x -= 1;
+                    break;
+            }
+
+            //check ship is in bounds
+            if (location.x <= world.x && location.y <= world.y && location.x >= 0 && location.y >= 0)
+            {
+                return true;
+            }
+
+            //goodbye ship - only want the first location that ship went out of bounds
+            if (alive)
+            {
+                lostLocation = location;
+            }
+            alive = false;
+            return alive;
         }
     }
 
@@ -57,8 +109,12 @@ namespace ShipsSurvey
             Coordinates world = new Coordinates() { x = 5, y = 3 };
             Ship ship1 = new Ship(world, 1, 1, (int)WorldHelper.CompassToRotation("e"));
 
-            Console.WriteLine(WorldHelper.RotationToCompass(ship1.rotation));
-            Console.WriteLine((int)WorldHelper.CompassToRotation("w"));
+            ship1.Left();
+            ship1.Forward();
+
+            Console.WriteLine(String.Format("x: {0} y: {1}", ship1.location.x, ship1.location.y));
+            //Console.WriteLine(WorldHelper.RotationToCompass(ship1.rotation));
+            //Console.WriteLine((int)WorldHelper.CompassToRotation("w"));
             Console.ReadLine();
         }
     }
